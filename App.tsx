5c99421e48e7bae7ec95c1841dbdf1e69844d09d
RSpannaus <rspannaus@seattleu.edu>
2026-03-04 18:24:55 -0800

    feat: add App.tsx with Kraken branding

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.header}>🏒 Kraken GameDay</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#001628',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    color: '#99D9D9',
+    fontSize: 28,
+    fontWeight: 'bold',
   },
 });
